7d52f76d26dfdc31875ec6d116f8dda8cf2dcd73
SeastarLI <649258445@qq.com>
2017-01-18 00:22:06 +0800

    1.预定义变量

--- 6tiao_jian_yu_ju.php
+++ 6tiao_jian_yu_ju.php
@@ -34,6 +34,23 @@ and open the template in the editor.
 	else:
 	    echo 'NO';
 	endif;
+	
+	//date($format[,$time=time()]);得到服务器的时间
+//	Y年m月d日H小时i分钟s秒w一周内的第多少天(0~6),0代表星期日
+	echo date('Y年m月d日 H:i:s 星期w');
+//	date_default_timezone_get():得到时区
+//	date_date_set($object, $year, $month, $day) 设置时区，还可以通过设置文件来设置时区
+	ini_get('date.timezone');
+	ini_set('date.timezone','Asia/Chongqing');
+	echo '<hr>',date_default_timezone_get();//Europe/Berlin 
+	date_default_timezone_set('PRC');//若设置成功，返回失去，否则返回false
+	//PRC中国时区
+	echo '<hr>',date_default_timezone_get();
+	echo '<hr>',date('Y年m月d日 H:i:s 星期w');
+	//通过time()函数得到当前时间截 秒数
+	echo '<hr>',time();
+	echo '<hr>',date("Y/m/d H:i:s",time()+24*3600);//意思是多一天
+	
 	?>
     </body>
 </html>
